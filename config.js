e268935976a8a44ec6a61cb192647b0fae4976e9
GOD ZENITSU <164893015+godzenitsu@users.noreply.github.com>
2024-04-26 18:44:17 +0530

    𝗤𝘂𝗲𝗲𝗻-𝗻𝗲𝘇𝘂𝗸𝗼

---
+++ config.js
@@ -0,0 +1,61 @@
+const fs = require('fs-extra')
+if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/config.env' })
+
+
+//═══════[Required Variables]════════\\
+global.owner = process.env.OWNER_NUMBER || '917907387121'  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
+global.mongodb = process.env.MONGODB_URI || "mongodb+srv://ahil1:787191784abhi@ahil1.kzr1tt3.mongodb.net/?retrywrites=true&w=majority"
+global.port= process.env.PORT || 5000
+global.email = 'maxdavid126@gmail.com'
+global.github = 'https://github.com/godzenitsu/QUEEN-NEZUKO-MD'
+global.location = 'Kerala,palakkad'
+global.gurl = 'https://instagram.com/whoois.zenitsu/' // add your username
+global.sudo = process.env.SUDO || "919744108970,917907387121,919605612983"
+global.devs = '2348069553870'
+global.website = 'https://github.com/godzenitsu/QUEEN-NEZUKO-MD' //wa.me/+917907387121
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://graph.org/file/171c1ceb7bd07b6160e12.jpg'
+module.exports = {
+  sessionName: process.env.SESSION_ID || "",      //Put Your Session Id Here
+  author:  process.env.PACK_AUTHER ||  'NEZUKO BOT',
+  packname:  process.env.PACK_NAME || 'MADE BY ZENITSU',
+  
+  botname:   process.env.BOT_NAME === undefined ? "NEZUKO-MD" : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? 'GOD-ZENITSU' : process.env.OWNER_NAME,  
+  auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
+  autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
+  antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
+  alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
+  antifake : process.env.FAKE_COUNTRY_CODE === undefined ? '234' : process.env.FAKE_COUNTRY_CODE,
+  readmessage:  process.env.READ_MESSAGE === undefined ? false : process.env.READ_MESSAGE,
+  auto_status_saver: process.env.AUTO_STATUS_SAVER === undefined ? false : process.env.AUTO_STATUS_SAVER,
+  HANDLERS:  process.env.PREFIX === undefined ? '.' : process.env.PREFIX,
+  warncount : process.env.WARN_COUNT === undefined ? 3 : process.env.WARN_COUNT,
+  disablepm:  process.env.DISABLE_PM === undefined ? false : process.env.DISABLE_PM,
+  levelupmessage:  process.env.LEVEL_UP_MESSAGE === undefined ? false : process.env.LEVEL_UP_MESSAGE,
+  antilink:  process.env.ANTILINK_VALUES === undefined ? 'chat.whatsapp.com' : process.env.ANTILINK_VALUES,
+  antilinkaction: process.env.ANTILINK_ACTION === undefined ? 'remove' : process.env.ANTILINK_ACTION,
+  BRANCH: 'main', 
+  ALIVE_MESSAGE:  process.env.ALIVE_MESSAGE === undefined ? '' : process.env.ALIVE_MESSAGE,
+  autobio:  process.env.AUTO_BIO === undefined ? false : process.env.AUTO_BIO,
+  caption :process.env.CAPTION || "\t*•ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɴᴇᴢᴜᴋᴏ-²²¹-ᴍᴅ•* ",   //*『ꜰᴏʟʟᴏᴡ ᴡʜᴏᴏɪꜱ.ᴢᴇɴɪᴛꜱᴜ』*\n www.instagram.com/whoois.zenitsu"),	
+  OPENAI_API_KEY:  process.env.OPENAI_API_KEY === undefined ? false : process.env.OPENAI_API_KEY,
+  heroku:  process.env.heroku === undefined ? false : process.env.heroku,
+  HEROKU: {
+    HEROKU: process.env.HEROKU ||false,
+    API_KEY: process.env.HEROKU_API_KEY === undefined ? '' : process.env.HEROKU_API_KEY,
+    APP_NAME: process.env.HEROKU_APP_NAME === undefined ? '' : process.env.HEROKU_APP_NAME
+},
+  VERSION: process.env.VERSION === undefined ? 'v.0.0.3' : process.env.VERSION,
+  LANG: process.env.THEME|| 'GOJO',
+  WORKTYPE: process.env.WORKTYPE === undefined ? 'public' : process.env.WORKTYPE
+};
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(`Update'${__filename}'`)
+    delete require.cache[file]
+	require(file)
+})
+ 
